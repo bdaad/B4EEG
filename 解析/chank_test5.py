953c7724d3167479842344d223ec1e3059ccd7c3
hayato <g2110237@edu.cc.uec.ac.jp>
2024-10-16 17:38:05 +0900

    更新

--- 解析/chank_test5.py
+++ 解析/chank_test5.py
@@ -613,12 +613,11 @@ def func_chank_10hz(receive_value, flag_blink, chank_list, clock_signal, adjust_
     # print("chank_list: ", chank_list)
     # テキストファイルにデータを追記
     # append_data_to_file(receive_data_txt, adjust_chank_list)
-        if po % 100 == 0:
+        if po % 100 == 0 and po != 0:
             print("len of chank_list 10Hz: ", len(chank_list))               
             # 各行の列数を出力
             for i, row in enumerate(chank_list):
-                # print(f"Row {i+1} length: {len(row)}")  # 各行の列数を出力
-                print(", ".join([f"{len(row)}" for row in chank_list]))
+                print(f"Row {i+1} length: {len(row)}")  # 各行の列数を出力
             
             # print("adjust_chank_list")
             # # 各行の列数を出力
@@ -691,13 +690,12 @@ def func_chank_12hz(receive_value, flag_blink, chank_list, clock_signal, adjust_
     # print("chank_list: ", chank_list)
     # テキストファイルにデータを追記
     # append_data_to_file(receive_data_txt, adjust_chank_list)
-        if po % 100 == 0:
+        if po % 100 == 0 and po != 0:
             print("len of chank_list 12Hz: ", len(chank_list))               
             # 各行の列数を出力
             for i, row in enumerate(chank_list):
-                # print(f"Row {i+1} length: {len(row)}")  # 各行の列数を出力
-                print(", ".join([f"{len(row)}" for row in chank_list]))
-
+                print(f"Row {i+1} length: {len(row)}")  # 各行の列数を出力
+                
             # print("adjust_chank_list")
             # # 各行の列数を出力
             # for i, row in enumerate(adjust_chank_list):
