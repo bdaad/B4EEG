ef54dafc8dcc45680ef7546ebde63dccfda73ea5
hayato <g2110237@edu.cc.uec.ac.jp>
2024-10-16 21:28:07 +0900

    更新

--- 解析/chank_test5.py
+++ 解析/chank_test5.py
@@ -663,7 +663,7 @@ def func_chank_12hz(receive_value, flag_blink, chank_list, clock_signal, adjust_
                         adjust_chank_list.append(adjust_data_to_size(chank_list_copy, target_size=83)) #1000data / 12Hz = 83.33333data
                     chank_chank_list_2 = []
                     po = po + 1
-                    # print("po: ", po)
+                    print("po: ", po)
                     # with lock:
                         # chank_list_copy = copy.deepcopy(list(chank_list[-3:])) #最後の3つのデータをコピー
                         # chank_list_copy = copy.deepcopy(list(chank_list[-1:])) #最後の1つのデータをコピー
