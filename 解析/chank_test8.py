537cbe83784d5adbcb9db313ed00a4dfc8cdf0e4
hayato <g2110237@edu.cc.uec.ac.jp>
2024-10-28 17:01:57 +0900

    更新

--- 解析/chank_test8.py
+++ 解析/chank_test8.py
@@ -205,8 +205,7 @@ def communicate_and_count_test(ser , received_list, receive_value, clock_signal_
             if result:
                 data_count += 1  # データをカウント
                 result = re.sub(rb'\r\n$', b'', result)  # 改行コードを削除
-                # result += b',0,0,'  # ',0,0,'を追加
-                print(result.decode())  # バイト列を文字列に変換
+                # result = result + b',0,0,'
                 try:
                     int_list_data = [int(x) for x in result.decode().split(',')]
                     int_list_data = iir_real_time_3ch(int_list_data, a, b, y_prev, x_prev)
