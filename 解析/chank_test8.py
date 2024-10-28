340561fcc6f9706d7f040f4406818206c070a8c7
hayato <g2110237@edu.cc.uec.ac.jp>
2024-10-28 16:47:05 +0900

    更新

--- 解析/chank_test8.py
+++ 解析/chank_test8.py
@@ -205,7 +205,7 @@ def communicate_and_count_test(ser , received_list, receive_value, clock_signal_
             if result:
                 data_count += 1  # データをカウント
                 result = re.sub(rb'\r\n$', b'', result)  # 改行コードを削除
-                result + b',0,0,'
+                result = result + b',0,0,'
                 try:
                     int_list_data = [int(x) for x in result.decode().split(',')]
                     int_list_data = iir_real_time_3ch(int_list_data, a, b, y_prev, x_prev)
@@ -1200,7 +1200,7 @@ def main():
         
     list_com()# COMポート一覧を表示
     # com = input_com()# COMポート接続の初期化
-    com = "COM3"
+    com = "COM8"
     # com = input_com()
     # print(com)
 
