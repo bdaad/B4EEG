6ac0195f56e9d0d77d805619ee5a20d32d2d7d49
hayato <g2110237@edu.cc.uec.ac.jp>
2024-10-28 16:59:36 +0900

    更新

--- 解析/chank_test8.py
+++ 解析/chank_test8.py
@@ -205,7 +205,7 @@ def communicate_and_count_test(ser , received_list, receive_value, clock_signal_
             if result:
                 data_count += 1  # データをカウント
                 result = re.sub(rb'\r\n$', b'', result)  # 改行コードを削除
-                result += b',0,0,'  # ',0,0,'を追加
+                # result += b',0,0,'  # ',0,0,'を追加
                 print(result.decode())  # バイト列を文字列に変換
                 try:
                     int_list_data = [int(x) for x in result.decode().split(',')]
